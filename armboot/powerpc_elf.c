dd07014f5b410ac457e456ce75450b1b2e869536
Maxternal <maxternal@gmail.com>
2013-07-31 20:06:59 -0600

    need to pay more attention to those warnings ... parenthesis

--- armboot/powerpc_elf.c
+++ armboot/powerpc_elf.c
@@ -664,8 +664,9 @@ int powerpc_boot_file(const char *path)
 	//fres = powerpc_load_dol("/bootmii/00000003.app", &endAddress);
 	//decryptionEndAddress = endAddress & ~3; 
 	//gecko_printf("powerpc_load_dol returned %d .\n", fres);
+	
 	gecko_printf("0xd8005A0 register value is %08x.\n", read32(0xd8005A0));
-	if(read32(0xd8005A0) & 0xFFFF0000 != 0xCAFE0000)
+	if((read32(0xd8005A0) & 0xFFFF0000) != 0xCAFE0000)
 	{	gecko_printf("Running old Wii code.\n");
 		flashSensor(500000, 500000, 500000);
 		powerpc_upload_oldstub(elfhdr.e_entry);
