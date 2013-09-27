52e7ba2f5f53dc520bde1e7f3cd3e032dbdabd2a
Maxternal <maxternal@gmail.com>
2013-09-26 21:09:44 -0600

    56 putting back core0 and commenting out the logfile debug since it doesn't seem to be working

--- armboot/gecko.c
+++ armboot/gecko.c
@@ -210,8 +210,8 @@ static int gecko_sendbuffer_safe(const void *buffer, u32 size)
 void gecko_init(void)
 {	if(read16(0x01200002) == 0XDEB6)
 		gecko_enabled |= 1;
-	if(f_open(&logFile, "/bootmii/log.txt", FA_CREATE_ALWAYS|FA_WRITE) == FR_OK)
-		gecko_enabled |= 2;
+//	if(f_open(&logFile, "/bootmii/log.txt", FA_CREATE_ALWAYS|FA_WRITE) == FR_OK)
+//		gecko_enabled |= 2;
 	write32(EXI0_CSR, 0);
 	write32(EXI1_CSR, 0);
 	write32(EXI2_CSR, 0);
@@ -255,10 +255,10 @@ int gecko_printf(const char *fmt, ...)
 	i = vsprintf(buffer, fmt, args);
 	va_end(args);
 	fmt = buffer;
-	if(gecko_enabled & 2)
+/*	if(gecko_enabled & 2)
 	{	f_puts(fmt, &logFile);
 		f_sync(&logFile);
-	}
+	}*/
 	if(gecko_enabled & 1)
 		while(*fmt)
 		{	/*do
