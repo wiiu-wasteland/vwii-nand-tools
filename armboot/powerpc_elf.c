78360c2e6f9c516fdc74f711ddfefd26310b29e1
Maxternal <maxternal@gmail.com>
2013-10-03 17:01:52 -0600

    it's commented out but we'll still add the fix from the armboot repo here

--- armboot/powerpc_elf.c
+++ armboot/powerpc_elf.c
@@ -623,7 +623,7 @@ int powerpc_boot_file(const char *path)
 	
 	gecko_printf("powerpc_load_elf returned %d .\n", fres = powerpc_load_elf(path));
 	//fres = powerpc_load_dol("/bootmii/00000003.app", &entry);
-	//decryptionEndAddress = ( 0x1330100 + read32(0x133008c) ) & ~3; 
+	//decryptionEndAddress = ( 0x1330100 + read32(0x133008c + read32(0x1330008)) -1 ) & ~3; 
 	//gecko_printf("powerpc_load_dol returned %d .\n", fres);
 	if(fres) return fres;
 	gecko_printf("0xd8005A0 register value is %08x.\n", read32(0xd8005A0));
